b5bef4c7efd28226bad0505c40a045695ed06778
fshinkae <felipeshinkae97@gmail.com>
2022-07-09 12:01:43 -0400

    Feat: Created migration, model and ajusted seeders by teams

---
+++ app/backend/src/database/models/ModelTeams.ts
@@ -0,0 +1,25 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
